95672e205f1480a65468aa8140bcb2b35f5eabc5
Mike D Sharp <msharp23@hotmail.co.uk>
2024-02-26 18:09:18 +0000

    fix: correct #ifdef typo

--- Engine/src/main.cpp
+++ Engine/src/main.cpp
@@ -9,9 +9,9 @@
     #include <SDL_mixer.h>
     #include <SDL_image.h>
 #else
-    #include <SDL.h>
-    #include <SDL_mixer.h>
-    #include <SDL_image.h>
+    #include <SDL2/SDL.h>
+    #include <SDL2/SDL_mixer.h>
+    #include <SDL2/SDL_image.h>
 #endif
 
 
